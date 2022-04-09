2a8feb47519bd7f220eb5f1454c92064a3f9cd4b
saketkum11 <kumarsaket601@gmail.com>
2022-04-09 12:08:39 +0530

    added:html for login and signup page

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -5,23 +5,48 @@ function Login() {
   const { auth } = useAuth();
   return (
     <>
-      <div className=" m-auto wt-30">
-        <form action="" className="bg-purple-8 flex flex-column m-t-9">
-          <label htmlFor="">Email</label>
+      <div className=" m-auto wt-30 flex flex-column justify-even">
+        <form
+          action=""
+          className="bg-black-1 text-color-9 justify-even flex flex-column  m-t-9 pd-11"
+        >
+          <label
+            htmlFor=""
+            className="flex items-start pd-y-5 pd-x-3 text-xm text-sm text-light"
+          >
+            Email
+          </label>
           <input
             type="email"
-            class=" pd-4  m-t-1 text-s rounded-s "
+            class=" pd-4   text-s rounded-xs "
             placeholder="Email Id"
           />
 
-          <label htmlFor="">Password </label>
+          <label
+            htmlFor=""
+            className="flex items-start text-xm pd-y-5 pd-x-3 text-sm text-light"
+          >
+            Password{" "}
+          </label>
+
           <input
-            type="email"
-            class="pd-4  m-t-1 text-s rounded-s "
-            placeholder="Email Id"
+            type="password"
+            class="pd-4 text-s rounded-xs outline-none "
+            placeholder="Password"
           />
-          <button>Login</button>
+          <button class="bg-purple-7 m-y-8 rounded-xs border-none outline-none text-s cursor text-color-0 pd-3 text-light">
+            Login
+          </button>
+
+          <button class="bg-black-0 rounded-xs outline-none pd-2 cursor">
+            Login as Guest{" "}
+          </button>
         </form>
+        <footer className="bg-black-4 ">
+          <a href="" class="text-dec rounded-xs text-color-0  pd-x-3 pd-y-2">
+            Create account
+          </a>
+        </footer>
       </div>
     </>
   );
--- src/Pages/Auth/Signup.jsx
+++ src/Pages/Auth/Signup.jsx
@@ -3,7 +3,66 @@ import React from "react";
 function Signup() {
   return (
     <>
-      <h1>This is Signup Page.</h1>
+      <div className=" m-auto wt-30 flex flex-column justify-even">
+        <form
+          action=""
+          className="bg-black-1 text-color-9 justify-even flex flex-column  m-t-9 pd-11"
+        >
+          <label
+            htmlFor=""
+            className="flex items-start pd-y-5 pd-x-3 text-xm text-sm text-light"
+          >
+            FirstName
+          </label>
+          <input
+            type="email"
+            class=" pd-4   text-s rounded-xs "
+            placeholder="eg: saket"
+          />
+          <label
+            htmlFor=""
+            className="flex items-start text-xm pd-y-5 pd-x-3 text-sm text-light"
+          >
+            LastName
+          </label>
+          <input
+            type="email"
+            class="pd-4 text-s rounded-xs outline-none "
+            placeholder="eg: kumar"
+          />
+          <label
+            htmlFor=""
+            className="flex items-start text-xm pd-y-5 pd-x-3 text-sm  text-light"
+          >
+            LastName
+          </label>
+          <input
+            type="email"
+            class="pd-4 text-s rounded-xs outline-none "
+            placeholder="eg: kumarsaket@gmail.com"
+          />
+          <label
+            htmlFor=""
+            className="flex items-start text-xm pd-y-5 pd-x-3 text-sm  text-light"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            class="pd-4 text-s rounded-xs outline-none "
+            placeholder="eg: asjh$dhdf12"
+          />
+          <button class="bg-purple-7 m-y-8 rounded-xs border-none outline-none text-s cursor text-color-0 pd-3 text-light">
+            Login
+          </button>
+          <a
+            href="#"
+            class="text-dec rounded-s text-color-9 bg-black-2 pd-x-3 pd-y-2"
+          >
+            Already have account
+          </a>
+        </form>
+      </div>
     </>
   );
 }
