2115cd897f55d3b75fde932dff5c039d81935106
saketkum11 <kumarsaket601@gmail.com>
2022-04-10 17:38:10 +0530

    modified:data

--- src/Context/Auth/Auth-Context.js
+++ src/Context/Auth/Auth-Context.js
@@ -5,23 +5,23 @@ const authContext = createContext();
 const useAuth = () => useContext(authContext);
 
 const AuthProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const signupHandler = async (data) => {
+  const signupHandler = async ({ firstName, lastName, email, password }) => {
     try {
       const response = await axios.post(`/api/auth/signup`, {
-        firstName: data.firstName,
-        lastName: data.lastName,
-        email: data.email,
-        password: data.password,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
       });
       // saving the encodedToken in the localStorage
       localStorage.setItem("token", response.data.encodedToken);
-      setUsers(response.data.encodedToken);
+
       console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
+
   return (
     <authContext.Provider value={{ signupHandler }}>
       {children}
