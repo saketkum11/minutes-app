7427cd1102f499d5afaa1bab79f9d4b349af60c3
saketkum11 <kumarsaket601@gmail.com>
2022-04-13 17:18:56 +0530

    added:color palett and label

--- src/Component/Card-note/CardNote.jsx
+++ src/Component/Card-note/CardNote.jsx
@@ -1,57 +1,112 @@
+import { useNote } from "../../Context/Note/note-context";
 import "../../Style/cardNote.css";
 function CardNote() {
+  const { getNotes, noteFooter, setNoteFooter } = useNote();
   return (
     <>
       {/* card component */}
       <section className=" m-y-5  wt-50 m-auto">
-        <div class="box-shadow pd-5 flex flex-column  m-y-8 rounded-s position-rel">
-          <div class="flex justify-btw pd-x-4 ">
-            <span className="text-m">Title</span>
-            <button class="text-color-5 bg-black-0 rounded-full outline-none border-none pd-x-3 pd-y-2 cursor">
-              <i class="fa-solid fa-thumbtack"></i>
+        <div className="box-shadow pd-5 flex flex-column  m-y-8 rounded-s position-rel">
+          <div className="flex justify-btw pd-x-4 ">
+            <input
+              type="text"
+              className="outline-none border-none text-lg pd-3"
+              placeholder="Title"
+            />
+            <button className="text-color-5 bg-black-0 rounded-full outline-none border-none pd-x-3 pd-y-2 cursor">
+              <i className="fa-solid fa-thumbtack"></i>
             </button>
           </div>
           <div className="flex items-start justify-around m-y-3">
-            <button class="text-color-5 cursor rounded-full border-none  outline-none pd-x-3 pd-y-2">
-              <i class="fa-solid fa-bold"></i>
+            <button className="text-color-5 cursor rounded-full border-none  outline-none pd-x-3 pd-y-2">
+              <i className="fa-solid fa-bold"></i>
             </button>
-            <button class="text-color-5 cursor rounded-full border-none  outline-none  pd-x-3 pd-y-2">
-              <i class="fa-solid fa-italic"></i>
+            <button className="text-color-5 cursor rounded-full border-none  outline-none  pd-x-3 pd-y-2">
+              <i className="fa-solid fa-italic"></i>
             </button>
-            <button class="text-color-5 cursor rounded-full border-none  outline-none text-color-0 pd-x-3 pd-y-2">
-              <i class="fa-solid fa-underline"></i>
+            <button className="text-color-5 cursor rounded-full border-none  outline-none text-color-0 pd-x-3 pd-y-2">
+              <i className="fa-solid fa-underline"></i>
             </button>
-            <button class="text-color-5 cursor rounded-full border-none  outline-none  pd-x-3 pd-y-2">
-              <i class="fa-solid fa-quote-right"></i>
+            <button className="text-color-5 cursor rounded-full border-none  outline-none  pd-x-3 pd-y-2">
+              <i className="fa-solid fa-quote-right"></i>
             </button>
-            <button class="text-color-5 cursor rounded-full border-none  outline-none  pd-x-3 pd-y-2">
-              <i class="fa-solid fa-text-slash"></i>
+            <button className="text-color-5 cursor rounded-full border-none  outline-none  pd-x-3 pd-y-2">
+              <i className="fa-solid fa-text-slash"></i>
             </button>
-            <button class="text-color-5 cursor rounded-full border-none  outline-none  pd-x-3 pd-y-2">
-              <i class="fa-solid fa-link"></i>
+            <button className="text-color-5 cursor rounded-full border-none  outline-none  pd-x-3 pd-y-2">
+              <i className="fa-solid fa-link"></i>
             </button>
           </div>
-          <div class="m-y-1 flex items-start pd-5">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
-              magnam.
-            </p>
+          <div className="m-y-1 flex items-start pd-5">
+            <input
+              type="text"
+              className="outline-none border-none text-m  pd-3"
+              placeholder="Write Your note"
+            />
           </div>
-          <div class="flex justify-even  items-start m-y-3 ">
-            <span>created 4 April'22</span>
-
-            <button class="text-color-5 cursor rounded-full border-none  outline-none text-color-0 pd-x-3 pd-y-2">
-              <i class="fa-solid fa-palette"></i>
+          <div className="flex justify-even  items-start m-y-3 ">
+            <button
+              onClick={() =>
+                setNoteFooter({ ...noteFooter, label: !noteFooter.label })
+              }
+              className="text-color-5 cursor rounded-full border-none  outline-none text-color-0 pd-x-3 pd-y-2"
+            >
+              <i className="fa-solid fa-tag"></i>
             </button>
 
-            <button class="bg-purple-7 rounded-s cursor border-none  outline-none text-color-0 pd-x-6 pd-y-3">
-              Add
+            <button
+              onClick={() => {
+                setNoteFooter({
+                  ...noteFooter,
+                  colorPalette: !noteFooter.colorPalette,
+                });
+              }}
+              className="text-color-5 cursor rounded-full border-none  outline-none text-color-0 pd-x-3 pd-y-2"
+            >
+              <i className="fa-solid fa-palette"></i>
             </button>
-            <button class="bg-pink-9 rounded-s border-none cursor outline-none text-color-0 pd-x-6 pd-y-3">
+
+            <button
+              onClick={() => {
+                getNotes();
+              }}
+              className="bg-purple-7 rounded-s cursor border-none  outline-none text-color-0 pd-x-6 pd-y-3"
+            >
+              Create
+            </button>
+            <button className="bg-pink-9 rounded-s border-none cursor outline-none text-color-0 pd-x-6 pd-y-3">
               Close
             </button>
           </div>
         </div>
+        {/*colorPalette*/}
+        {noteFooter.colorPalette ? (
+          <div className="bg-black-1 flex justify-even  wt-20 flex-column">
+            <div className="flex justify-even pd-y-2">
+              <button className="h-fixed-4 cursor wt-fixed-4  bg-pink-9  rounded-full border-none   "></button>
+              <button className=" h-fixed-4 cursor wt-fixed-4 bg-purple-9  rounded-full border-none outline-none  "></button>
+              <button className="h-fixed-4 cursor wt-fixed-4  bg-green-9  rounded-full border-none  outline-none  "></button>
+            </div>
+            <div className="flex justify-even pd-y-2">
+              <button className="h-fixed-4 cursor wt-fixed-4  bg-blue-9  rounded-full border-none  outline-none  "></button>
+              <button className="h-fixed-4 cursor wt-fixed-4  bg-pink-5  rounded-full border-none  outline-none  "></button>
+              <button className="h-fixed-4 cursor wt-fixed-4  bg-red-5  rounded-full border-none  outline-none  "></button>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        {/*Label*/}
+        {noteFooter.label ? (
+          <div className="bg-black-1 flex flex-wrap pd-2 wt-20  flex-column">
+            <input type="text" className="wt-80 m-auto m-y-2 " onChange />
+            <button className="bg-purple-7  rounded-s border-none wt-50 m-auto outline-none text-color-0 pd-x-3 pd-y-2 m-y-2">
+              Label
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
       </section>
     </>
   );
