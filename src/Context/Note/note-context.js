2de30ba039b524cc68cfddef32c8fbe64837928a
saketkum11 <kumarsaket601@gmail.com>
2022-04-13 11:25:47 +0530

    added:api call

--- src/Context/Note/note-context.js
+++ src/Context/Note/note-context.js
@@ -1,10 +1,27 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useContext } from "react";
 import { useAuth } from "../Auth/Auth-Context";
+import axios from "axios";
 const noteContext = createContext();
 const useNote = () => useContext(noteContext);
 
 const NoteProvider = ({ children }) => {
-  const {} = useAuth();
+  const { tokenStorages } = useAuth();
+
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        const response = await axios.get("/api/notes", {
+          headers: {
+            authorization: tokenStorages,
+          },
+        });
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getNotes();
+  }, []);
 
   return (
     <noteContext.Provider value={{ set: 0 }}>{children}</noteContext.Provider>
